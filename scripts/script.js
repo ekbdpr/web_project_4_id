49a0f4f70479cbd9b9671302ab003942cc56c6e1
Akemi <eka.budhiprasetya@gmail.com>
2023-03-18 21:07:12 +0800

    refactor: script based on DRY

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,106 @@
-// declare variables
-let container = document.querySelector(".content");
-let page = document.querySelector(".pages");
-let editButton = document.querySelector(".profile__btn-edit");
-let editFormOpen = document.querySelector(".edit");
-let editFormClose = document.querySelector(".edit__close");
-let submitButton = document.querySelector(".edit__submit");
-let editUsername = document.querySelector(".edit__username");
-let editJob = document.querySelector(".edit__job");
-let userName = document.querySelector(".profile__username");
-let job = document.querySelector(".profile__job");
+// initial cards
+const initialCards = [
+  {
+    name: "Lembah Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Danau Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Pegunungan Gundul",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Gunung Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Taman Nasional Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
 
-// function handlers
-function editProfile() {
-  editUsername.value = userName.textContent;
-  editJob.value = job.textContent;
+// define global variables
+const mainBody = document.querySelector(".content");
+const page = document.querySelector(".pages");
+const editProfileBtn = document.querySelector(".profile__btn-edit");
+const maxChars = 20;
+
+// add event listeners
+editProfileBtn.addEventListener("click", () => {
+  showPopUp("#editProfileTemp", ".edit", ".edit__close");
+});
+
+// show popUp window handler
+function showPopUp(element, elementClass, closeElement) {
+  const templateId = document.querySelector(element).content;
+  const templateNode = templateId.querySelector(elementClass).cloneNode(true);
+
+  mainBody.append(templateNode);
+  profileData(templateNode);
+
+  setTimeout(() => {
+    templateNode.classList.add("popUp");
+    page.classList.add("pages_dimmed");
+    mainBody.setAttribute("style", "overflow: hidden; padding-right: 5px");
+  }, 5);
+
+  const removeElement = document.querySelector(closeElement);
+
+  removeElement.addEventListener("click", () => {
+    removePopUp(templateNode);
+  });
 }
 
-function showEditForm() {
-  editFormOpen.classList.add("edit_activate");
-  page.classList.add("pages_dimmed");
-  container.setAttribute("style", "overflow: hidden");
-
-  editProfile();
+// remove popUp window
+function removePopUp(templateNode) {
+  templateNode.style.opacity = 0;
+  setTimeout(() => {
+    templateNode.remove();
+    page.classList.remove("pages_dimmed");
+    mainBody.removeAttribute("style");
+  }, 300);
 }
 
-function hideEditForm() {
-  editFormOpen.classList.remove("edit_activate");
-  page.classList.remove("pages_dimmed");
-  container.removeAttribute("style", "overflow: hidden");
+// function to copy text content to value
+function profileData(templateNode) {
+  const userName = document.querySelector(".profile__username");
+  const userJob = document.querySelector(".profile__job");
+  const editUserName = document.querySelector(".edit__username");
+  const editUserJob = document.querySelector(".edit__job");
+  const submitButton = document.querySelector(".edit__submit");
+
+  setTimeout(() => {
+    editUserName.value = userName.textContent;
+    editUserJob.value = userJob.textContent;
+    submitButton.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      updateProfile(userName, userJob, editUserName, editUserJob, templateNode);
+    });
+  }, 1);
 }
 
-function submitForm() {
-  event.preventDefault();
-  let maxChars = 20;
-  userName.textContent = editUsername.value.slice(0, maxChars);
-  job.textContent = editJob.value.slice(0, maxChars);
+// update user profile data
+function updateProfile(
+  userName,
+  userJob,
+  editUserName,
+  editUserJob,
+  templateNode
+) {
+  userName.textContent = editUserName.value.slice(0, maxChars);
+  userJob.textContent = editUserJob.value.slice(0, maxChars);
 
-  if (userName.length > maxChars || job.length > maxChars) {
+  if (userName.length > maxChars || userJob.length > maxChars) {
     alert("Max chars exceeded");
     return;
   }
 
-  hideEditForm();
+  removePopUp(templateNode);
 }
-
-function enterSubmit() {
-  if (event.keyCode === 13) {
-    submitForm();
-  }
-}
-
-// event listeners
-editButton.addEventListener("click", showEditForm);
-editFormClose.addEventListener("click", hideEditForm);
-submitButton.addEventListener("click", submitForm);
-editFormOpen.addEventListener("keypress", enterSubmit);
