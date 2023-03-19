d0e657457950a1b6ced8d230351f4c36618aeb53
Akemi <eka.budhiprasetya@gmail.com>
2023-03-19 09:09:54 +0800

    feature: adding functionality to heart button

--- scripts/script.js
+++ scripts/script.js
@@ -26,11 +26,10 @@ const initialCards = [
   },
 ];
 
-// define global variables
+// define global variables for popUp
 const mainBody = document.querySelector(".content");
 const page = document.querySelector(".pages");
 const editProfileBtn = document.querySelector(".profile__btn-edit");
-const maxChars = 20;
 
 // add event listeners
 editProfileBtn.addEventListener("click", () => {
@@ -48,7 +47,7 @@ function showPopUp(element, elementClass, closeElement) {
   setTimeout(() => {
     templateNode.classList.add("popUp");
     page.classList.add("pages_dimmed");
-    mainBody.setAttribute("style", "overflow: hidden; padding-right: 5px");
+    mainBody.setAttribute("style", "overflow: hidden");
   }, 5);
 
   const removeElement = document.querySelector(closeElement);
@@ -94,6 +93,8 @@ function updateProfile(
   editUserJob,
   templateNode
 ) {
+  const maxChars = 20;
+
   userName.textContent = editUserName.value.slice(0, maxChars);
   userJob.textContent = editUserJob.value.slice(0, maxChars);
 
@@ -104,3 +105,19 @@ function updateProfile(
 
   removePopUp(templateNode);
 }
+
+// define global variables for heart button
+const heartActive = document.querySelectorAll(".element__heart-btn");
+
+// heart button handler
+heartActive.forEach((element) => {
+  element.addEventListener("click", function () {
+    let isActive = element.classList.contains("element__heart_active");
+
+    if (!isActive) {
+      element.classList.add("element__heart_active");
+    } else {
+      element.classList.remove("element__heart_active");
+    }
+  });
+});
